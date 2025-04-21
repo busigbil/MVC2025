ea4ebdf9cf9a7d44f808b329c25a7e9d8e1fceb3
Jenny Karlsson <jenny.ec.karlsson@gmail.com>
2025-04-21 17:49:38 +0200

    1.0.0.

---
+++ src/Controller/ApiJson.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\RouterInterface;
+
+
+class ApiJson extends AbstractController
+{
+    #[Route("/api", name: 'json_routes')]
+    public function jsonRoutes(RouterInterface $router): Response
+    {
+        //Get all routes.
+        $routes = $router->getRouteCollection()->all();
+        $apiArr = array();
+
+        //Add route to array if starts with api.
+        foreach ($routes as $route) {
+            if (str_starts_with($route->getPath(), '/api/')) {
+                $apiArr[] = basename($route->getPath());
+            }
+        }
+
+        $data = [
+            'routes' => $apiArr
+        ];
+        return $this->render('api.html.twig', $data);
+    }
+
+    #[Route("/api/quote")]
+    public function jsonQuote(): Response
+    {
+
+        //Create quote and date.
+        $quoteArr = array(
+            'Människan är alltings mått. - Protagoras',
+            'Att lära många ting lär oss inte att förstå. - Herakleitos',
+            'Svin njuter av gyttja mer än rent vatten. - Herakleitos',
+            'Den som vill försöka bli lycklig skall inte lägga an på att öka sina tillgångar utan på att begränsa sina krav. - Platon',
+            'En god och ädel man bör avhålla sig från varje som helst kontakt med arbetslivet. - Platon',
+            'Om jag ändå kunde finna sanningen lika lätt som jag kan avslöja osanningen. - Cicero'
+        );
+
+        $index = random_int(0, count($quoteArr) -1);
+
+        date_default_timezone_set("Europe/Stockholm");
+        $dateTime = date("Y-m-d H:i:s", time());
+
+        //Add quote and date for json response.
+        $data = [
+            'today-quote' => $quoteArr[$index],
+            'today-date' => $dateTime
+        ];
+
+        $response = new JsonResponse($data);
+        $response->setEncodingOptions(
+            $response->getEncodingOptions() | JSON_PRETTY_PRINT
+        );
+        return $response;
+    }
+}
